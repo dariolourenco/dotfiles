bbd8cb6d9b9e2c97d5d15ba6f4039c26c74691f8
Dário Lourenço <2058655+darioxfz@users.noreply.github.com>
2024-04-27 16:55:51 +0100

    remove onedrive and install wsl

--- setup.ps1
+++ setup.ps1
@@ -1,42 +1,63 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    # Relaunch the script with administrator rights
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-   # Start-Process powershell -Verb runAs -ArgumentList $arguments
-    #Exit
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  # Relaunch the script with administrator rights
+  $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+  # Start-Process powershell -Verb runAs -ArgumentList $arguments
+  #Exit
 }
 
+function Remove-One-Drive {
+  [OutputType([bool])]
+  param()
+  Write-Output "Removing One Drive"
+  Stop-Process -Name "OneDrive" -Force
+  Start-Process "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe" /uninstall
+}
 
-function Install-Scoop{
-  $installed = $null
-  try{
-      $installed = Get-Command scoop -ErrorAction Stop
-  }catch{
-        Write-Host "scoop is not Installed"
+function Install-WSL {
+  [OutputType([void])]
+  param()
+  Write-Output "Installing WSL"
+  wsl --install Ubuntu -n
+}
+
+function Is-Application-Installed {
+  [OutputType([bool])]
+  param(
+    [string]$applicationName
+  )
+  if (Get-Command $applicationName -ErrorAction SilentlyContinue) {
+    return $true
   }
-  if($null -eq $installed){
-      Write-Host "installing scoop"
-      Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-      Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression 
+  else {
+    return $false
   }
-  else{
-      Write-Host "scoop already installing. Updating scoop"
-      scoop update
-    }
+}
+
+function Install-Scoop {
+  if (Is-Application-Installed "scoop" -eq $false) {
+    Write-Host "installing scoop"
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression 
   }
-function Add-Scoop-Buckets{
+  else {
+    Write-Host "scoop already installing. Updating scoop"
+    scoop update
+  }
+}
+
+function Add-Scoop-Buckets {
   scoop bucket add stripe https://github.com/stripe/scoop-stripe-cli.git
   scoop bucket add extras
   scoop bucket add nerd-fonts
 }
 
-function Install-With-Winget{
-    param(
+function Install-With-Winget {
+  param(
     [string]$packageName
-    )
+  )
   $wingetInstallCommand = "winget install $packageName -e"
   Invoke-Expression -Command $wingetInstallCommand
-  }
+}
 
 
 Write-Host "[>] Installing Scoop"
@@ -49,9 +70,10 @@ scoop install Firacode-NF
 scoop install fzf
 scoop install bat
 scoop install ripgrep
-scoop install nvim
+#scoop install nvim
 scoop install neovide
 
+Remove-One-Drive
 #scoop update *
 #Install-With-Winget "chocolatey"
 #Install-With-Winget "vim.vim"
